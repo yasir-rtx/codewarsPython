5992aff1a29347dd5c1a1f9d8bf5e306f7bfe922
yasir-rtx <yasir112358@gmail.com>
2024-02-24 14:38:20 +0700

    Categorize New Member
    24 February

---
+++ 02-February/#24_1.py
@@ -0,0 +1,13 @@
+# Categorize New Member
+# https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/python
+
+
+def open_or_senior(data):
+    categories = []
+    for x, y in data: categories.append("Senior") if (x >= 55 and y > 7) else categories.append("Open")
+    return categories
+
+
+# print(open_or_senior([(45, 12),(55,21),(19, -2),(104, 20)]))
+print(open_or_senior([(45, 12), (55, 21), (19, -2), (104, 20)]))
+print(open_or_senior([(16, 23),(73,1),(56, 20),(1, -1)]))
